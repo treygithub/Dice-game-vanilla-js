224c68121e8ec4823a9812e48b5c7282c42fc637
Robert Hunnicutt <robhunn74@gmail.com>
2019-02-02 22:30:56 -0600

    added some queryselectors and eventlistener

--- app.js
+++ app.js
@@ -6,3 +6,25 @@ GAME RULES:
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
 */
+
+let scores,roundScores,activePlayer,dice;
+
+scores = [0,0];
+roundScores = 0;
+activePlayer = 0;
+
+document.querySelector('.dice').style.display = 'none';
+
+document.querySelector('.btn-roll').addEventListener('click', function(){
+    let dice = Math.floor(Math.random() * 6) + 1;
+    let diceDOM = document.querySelector(".dice");
+    diceDOM.style.display =  "block";
+    diceDOM.src = "/img/dice-" + dice + ".png";
+});
+
+//document.querySelector("#current-" + activePlayer).textContent = dice;
+//document.querySelector("#current-" + activePlayer).innerHTML = `<em> ${dice} </em>`;
+//document.querySelector('.btn-roll').addEventListener('click', btn);
+//function btn(){
+    //do somethinig
+//}
