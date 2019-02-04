007baccb0537ec833f4ea3d612a89ac523ebf8a5
Robert Hunnicutt <robhunn74@gmail.com>
2019-02-04 17:43:03 -0600

    dom manipulation

--- app.js
+++ app.js
@@ -16,11 +16,38 @@ activePlayer = 0;
 document.querySelector('.dice').style.display = 'none';
 
 document.querySelector('.btn-roll').addEventListener('click', function(){
+    //random number
     let dice = Math.floor(Math.random() * 6) + 1;
+
+    // Display the results
     let diceDOM = document.querySelector(".dice");
     diceDOM.style.display =  "block";
     diceDOM.src = "/img/dice-" + dice + ".png";
+
+    //update score if number rolled was not one
+    if(dice !== 1 ){
+        roundScores += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScores;
+    }else{
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScores = 0;
+        document.getElementById('current-0').textContent = 0;
+        document.getElementById('current-1').textContent = 0;
+
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+        document.querySelector('.dice').style.display = 'none';
+    }
 });
+
+document.querySelector('.btn-hold').addEventListener('click',function(){
+    //add current score to global score
+    scores[activePlayer] += roundScores
+    //update the ui
+    document.querySelector('#score-' + activePlayer).textContent = score
+    //
+})
 
 //document.querySelector("#current-" + activePlayer).textContent = dice;
 //document.querySelector("#current-" + activePlayer).innerHTML = `<em> ${dice} </em>`;
